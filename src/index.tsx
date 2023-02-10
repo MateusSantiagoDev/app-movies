04d722b47600142286514505619d90c8cbcca5b3
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-10 10:16:01 -0300

    add app component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { App } from './app'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <h1>ola</h1>
+    <App/>
   </React.StrictMode>
 )
