dab592c49adffce8ebba7b0ef1291619402b1b11
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-10 10:47:08 -0300

    feat: add react router dom

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,12 @@
+import { LoginForm } from "./components/form-login/loginForm"
+import { BrowserRouter } from "react-router-dom"
+
 export function App () {
   return (
      <div>
-        <h1>ola !!</h1>
+        <BrowserRouter>
+        <LoginForm/>
+        </BrowserRouter>
      </div>
   )
 }
